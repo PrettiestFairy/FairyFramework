46a7d8897522ce9f7510030f7d2dfdc0908b3932
AustinFairyland <fairylandhost@outlook.com>
2023-10-12 07:24:36 +0800

    :bulb:annotate

--- modules/journals/base.py
+++ modules/journals/base.py
@@ -20,12 +20,9 @@ warnings.filterwarnings('ignore')
 if platform.system() == 'Windows':
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
-import time
-import random
-
 from loguru import logger
 
-from utils.publics import PublicUtilsBaseClass
+from tools.publics import PublicToolsBaseClass
 
 
 class JournalsModuleClass:
@@ -35,7 +32,11 @@ class JournalsModuleClass:
         self.__config_logger()
 
     def __config_logger(self):
-        logs_path = PublicUtilsBaseClass.conver_slach(os.path.join(PublicUtilsBaseClass.root_path, 'logs/services.log'))
+        """
+        logger 配置
+        :return: Object logger对象
+        """
+        logs_path = PublicToolsBaseClass.conver_slach(os.path.join(PublicToolsBaseClass.root_path, 'logs/services.log'))
         logger.add(
             sink=logs_path,
             rotation='10 MB',
@@ -52,31 +53,96 @@ class JournalsModuleClass:
 
     @property
     def __logs(self):
+        """
+        私有方法 logger
+        :return: Object logger对象
+        """
         return logger
-
+    
+    @property
     def catch(self):
+        """
+        继承 loguru.logger 的 catch 方法
+        :return: Object loguru.logger.catch 方法
+        """
         return self.__logs.catch()
 
     def trace(self, msg, *args, **kwargs):
+        """
+        继承 loguru.logger 的 trace 方法
+        :param msg: String 日志信息
+        :param args: args
+        :param kwargs: kwargs
+        :return: Object loguru.logger.trace 方法
+        """
         return self.__logs.trace(msg, *args, **kwargs)
 
     def debug(self, msg, *args, **kwargs):
+        """
+        继承 loguru.logger 的 debug 方法
+        :param msg: String 日志信息
+        :param args: args
+        :param kwargs: kwargs
+        :return: Object loguru.logger.debug 方法
+        """
         return self.__logs.debug(msg, *args, **kwargs)
 
     def info(self, msg, *args, **kwargs):
+        """
+        继承 loguru.logger 的 info 方法
+        :param msg: String 日志信息
+        :param args: args
+        :param kwargs: kwargs
+        :return: Object loguru.logger.info 方法
+        """
         return self.__logs.info(msg, *args, **kwargs)
 
     def success(self, msg, *args, **kwargs):
+        """
+        继承 loguru.logger 的 success 方法
+        :param msg: String 日志信息
+        :param args: args
+        :param kwargs: kwargs
+        :return: Object loguru.logger.success 方法
+        """
         return self.__logs.success(msg, *args, **kwargs)
 
     def warning(self, msg, *args, **kwargs):
+        """
+        继承 loguru.logger 的 warning 方法
+        :param msg: String 日志信息
+        :param args: args
+        :param kwargs: kwargs
+        :return: Object loguru.logger.warning 方法
+        """
         return self.__logs.warning(msg, *args, **kwargs)
 
     def error(self, msg, *args, **kwargs):
+        """
+        继承 loguru.logger 的 error 方法
+        :param msg: String 日志信息
+        :param args: args
+        :param kwargs: kwargs
+        :return: Object loguru.logger.error 方法
+        """
         return self.__logs.error(msg, *args, **kwargs)
 
     def critical(self, msg, *args, **kwargs):
+        """
+        继承 loguru.logger 的 critical 方法
+        :param msg: String 日志信息
+        :param args: args
+        :param kwargs: kwargs
+        :return: Object loguru.logger.critical 方法
+        """
         return self.__logs.critical(msg, *args, **kwargs)
 
     def exception(self, msg, *args, **kwargs):
+        """
+        继承 loguru.logger 的 exception 方法
+        :param msg: String 日志信息
+        :param args: args
+        :param kwargs: kwargs
+        :return: Object loguru.logger.exception 方法
+        """
         return self.__logs.exception(msg, *args, **kwargs)
