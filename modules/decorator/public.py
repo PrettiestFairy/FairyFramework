80c427b4d7cec56ce9d49e56a5ae901eb2ed7b9d
Austin <fairylandhost@outlook.com>
2024-01-04 10:12:08 +0800

    :art: decorators

--- modules/decorator/public.py
+++ modules/decorator/public.py
@@ -20,13 +20,15 @@ if platform.system() == "Windows":
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
 import time
+from typing import Callable, Mapping
+
 from modules.journals import InitJournalModulesClass
 
 
 class TimingDecorator:
     """Runtime Decorator"""
 
-    def __call__(self, func):
+    def __call__(self, func, *args, **kwargs):
         def warpper(*args, **kwargs):
             start_time = time.time()
             result = func(*args, **kwargs)
@@ -38,3 +40,49 @@ class TimingDecorator:
             return result
 
         return warpper
+
+
+class MethodRunningAnnotateDecorator:
+    def __init__(self, annotate: str, *args, **kwargs):
+        self.annotate: str = annotate
+
+    @InitJournalModulesClass.journal.catch()
+    def __call__(self, method: Callable[..., ...], *args, **kwargs):
+        def warpper(*args, **kwargs):
+            try:
+                InitJournalModulesClass.journal.info("")
+                warpper(*args, **kwargs)
+                InitJournalModulesClass.journal.success("")
+            except Exception as error:
+                print(error)
+                InitJournalModulesClass.journal.error("")
+
+        return warpper
+
+
+class MethodDecorators:
+    def __init__(self, annotation):
+        self.__annotation = annotation
+
+    @TimingDecorator()
+    def __call__(self, function, *args, **kwargs):
+        def wrapper(*args, **kwargs):
+            print("Start Running {}".format(self.__annotation))
+            results = None
+            try:
+                results = function(*args, **kwargs)
+            except Exception as error:
+                print(error)
+            return results
+
+        return wrapper
+
+
+@MethodDecorators("123")
+def run():
+    # print("run...")
+    return True
+
+
+if __name__ == "__main__":
+    print(run())
